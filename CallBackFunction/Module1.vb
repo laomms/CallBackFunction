ea8fabca82083e50fbc679a830fb5ffb4cf1efba
Tony <laomms@gmail.com>
2020-02-20 12:17:10 +0700

    Add files via upload

---
+++ CallBackFunction/Module1.vb
@@ -0,0 +1,17 @@
+﻿Imports ClassLibrary1
+
+Module Module1
+
+    Sub Main()
+        Dim dll As New Class1()
+        dll.SetAddCallBack(AddressOf Add) ' 设置回调
+        dll.CallAdd() ' 触发回调
+        Console.Read() ' 暂停程序
+    End Sub
+    Private Function Add(ByVal a As Integer, ByVal b As Integer) As Integer
+        Dim c As Integer = a + b
+        Console.WriteLine("Add被调用了，a+b={0}", c)
+        Return c
+    End Function
+
+End Module
