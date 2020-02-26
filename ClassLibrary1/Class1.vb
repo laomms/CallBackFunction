c98c294e4e8e4c11bf2eaffbfe0c815968bb358f
Tony <laomms@gmail.com>
2020-02-26 09:09:04 +0700

    Add files via upload

---
+++ ClassLibrary1/Class1.vb
@@ -0,0 +1,17 @@
+﻿Public Class Class1
+
+
+
+	Public Delegate Sub DGetResult(szRes As String)
+	Public Shared GetResult As DGetResult = Nothing ' 此处相当于定义函数指针了
+	' 设置回调函数地址
+	Public Shared Sub GetResultCallBack(ByVal GetResultFunc As DGetResult)
+		GetResult = GetResultFunc
+	End Sub
+
+	' 调用回调函数
+	Public Shared Sub CallGetResult(szRes As String)
+		CallGetResults(szRes)
+	End Sub
+
+End Class
---
+++ ClassLibrary1/Module1.vb
@@ -0,0 +1,11 @@
+﻿Module Module1
+	Public Sub CallGetResults(szRes As String)
+		If Class1.GetResult IsNot Nothing Then
+			szRes = "123456"
+			Class1.GetResult(szRes)
+			Class1.GetResult("b")
+			Class1.GetResult("c")
+		End If
+	End Sub
+
+End Module
